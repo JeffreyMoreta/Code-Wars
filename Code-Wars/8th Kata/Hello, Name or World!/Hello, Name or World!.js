a0c5923a73c11ecd8a930427a0801928d4356309
JeffreyMoreta <jeffreymoreta@gmail.com>
2020-08-13 10:56:17 -0400

    Accidental duplicate

--- Code-Wars/8th Kata/Hello, Name or World!/Hello, Name or World!.js
+++
@@ -1,8 +0,0 @@
-function hello(name) {
-  if (name === undefined || name === ""){
-    return "Hello, World!";
-  }else{
-    name = name.slice(0,1).toUpperCase() + name.slice(1).toLowerCase();
-    return `Hello, ${name}!`;
-  }
-};
